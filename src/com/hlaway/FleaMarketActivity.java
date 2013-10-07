85ec6682b24086c36d02a46be8f26f50eed991eb
hl-away <hl-away@tut.by>
2013-10-08 00:23:45 +0300

    init project

---
+++ src/com/hlaway/FleaMarketActivity.java
@@ -0,0 +1,15 @@
+package com.hlaway;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FleaMarketActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
